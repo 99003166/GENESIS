76f1d6d87b62db2299b432f07cbe95e35f1dcf16
Stepin104877 <69139953+Stepin104877@users.noreply.github.com>
2020-09-21 12:50:41 +0530

    Update test_covid_operations.c

--- test/test_covid_operations.c
+++ test/test_covid_operations.c
@@ -3,7 +3,7 @@
 
 /* Modify these two lines according to the project */
 #include <covid_operations.h>
-#define PROJECT_NAME    "covid"
+#define PROJECT_NAME    "Covid"
 
 /* Prototypes for all the test functions */
 void test_visits(void);
