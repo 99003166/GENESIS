b8f884c0830a2c6ad1df1ff629ee0d2a0eb2c478
Stepin104877 <69139953+Stepin104877@users.noreply.github.com>
2020-09-21 13:47:54 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -4,18 +4,18 @@
 #define VALID   (1)
 #define INVALID (0)
 
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
+/* Covid operation requested by user*/
+unsigned int covid_operation = 0;
 
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
+/* Operands on which operation count is performed */
+int covid_operand1 = 0;
+int covid_operand2 = 0;
 
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
+/* Covid operations */
+enum operations{ VISITS=1, POSITIVE=1, NEGATIVE, DIED=1, RECOVERED, HOMEQUARANT=1, REVISIEDPOS=1, REVISITEDNEG, HOSPQUARANT=1, COMPISOLATION=1, DIFFSTATEPOS=1, DIFFSTATENEG, PRIMECON=1, SECCON=1, INFECTEDRATE, DEATHRATE, RECOVERYRATE, EXIT };
 
 /* Display the menu of operations supported */
-void calculator_menu(void);
+void covid_menu(void);
 /* Verifies the requested operations validity */
 int valid_operation(int operation);
 
@@ -23,31 +23,31 @@ int valid_operation(int operation);
 /* Start of the application */
 int main(int argc, char *argv[])
 {
-    printf("\n****Calculator****\n");
+    printf("\n****COVID MANAGEMENT****\n");
     while(1)
     {
-        calculator_menu();
+        covid_menu();
     }
 }
 
-void calculator_menu(void)
+void covid_menu(void)
 {
     printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
+    printf("\n1. Visits \n2. Positive\n3. Negative\n4. Died\n5. Recovered\n6. Homequarant\n7. Revisitedpos\n8. Revisitedneg\n9. Hospquarant\n10. Compisolation\n11. Diffstatepos\n12. Diffstateneg\n13. Primecon\n14. Seconcon\n14. Infectedrate\n15. Deathrate\n16. Recoveryrate\n17.");
     printf("\n\tEnter your choice\n");
    
      __fpurge(stdin);
-    scanf("%d", &calculator_operation);
+    scanf("%d", &covid_operation);
 
-    if(EXIT == calculator_operation)
+    if(EXIT == covid_operation)
     {
         printf("\nThank you. Exiting the Application\n");
         exit(0);
     }
 
-    if(INVALID != valid_operation(calculator_operation))
+if(INVALID != valid_operation(covid_operation))
     {
-        printf("\n\tEnter your Numbers with space between them\n");
+        printf("\n\tEnter the new and previous count with space between them\n");
         __fpurge(stdin);
         scanf("%d %d", &calculator_operand1, &calculator_operand2);
     }
@@ -59,54 +59,172 @@ void calculator_menu(void)
         return;
         
     }
-    switch(calculator_operation)
+    
+    switch(covid_operation)
     {
-        case ADD:
+        case VISITS:
             printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
+            covid_operand1, 
+            covid_operand2,
+           visits(covid_operand1, covid_operand2));
             
             __fpurge(stdin);
             getchar();
             break;
-        case SUBTRACT:
+            case POSITIVE:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           positive(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+         case NEGATIVE:
             printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
+            covid_operand1, 
+            covid_operand2,
+           negative(covid_operand1, covid_operand2));
             
             __fpurge(stdin);
             getchar();
             break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
+             case DIED:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           died(covid_operand1, covid_operand2));
             
             __fpurge(stdin);
             getchar();
             break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
+             case RECOVERED:
+            printf("\n\t%d - %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           recovered(covid_operand1, covid_operand2));
             
             __fpurge(stdin);
             getchar();
             break;
-        case 5:
+             case HOMEQUARANT:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           homequarant(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+             case REVISITEDPOS:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           revisitedpos(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case REVISITEDNEG:
+            printf("\n\t%d - %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           revisitedneg(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case HOSPQUARANT:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           hospquarant(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case COMPISOLATION:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           compisolation(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case DIFFSTATEPOS:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           diffstatepos(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case DIFFSTATENEG:
+            printf("\n\t%d - %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           diffstateneg(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case PRIMECON:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           primecon(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case SECCON:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            covid_operand1, 
+            covid_operand2,
+           seccon(covid_operand1, covid_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+                case INFECTEDRATE:
+            printf("\n\t(%d / %d)*100 = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            infectedrate(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case DEATHRATE:
+            printf("\n\t(%d / %d)*100 = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            deathrate(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+            case RECOVERYRATE:
+            printf("\n\t(%d / %d)*100 = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            recoveryrate(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 18:
             exit(0);
             break;
         default:
-            printf("\n\t---It should never come here---\n");
+            printf("\n\t---No counts---\n");
     }
 }
 
 int valid_operation(int operation)
 {
     /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+    return ((VISITS <= operation) && (EXIT >= operation)) ? VALID: INVALID;
 }
